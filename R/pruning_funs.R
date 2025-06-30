739d65c3d0926af19688ff5e361a8f95e9d3b2b7
Haoyu Ji <alanji0322@qq.com>
2025-06-30 05:12:28 -0400

    4by4 done

--- R/pruning_funs.R
+++ R/pruning_funs.R
@@ -1,3 +1,46 @@
+#' @param n (vector of) number of states (if n is a scalar, all traits have the same number of states)
+#' @param k number of traits (only used if n is a scalar)
+#' @examples
+#' setup_Q_template(n = 3, k = 2)
+#' setup_Q_template(n = 2, k = 3)
+setup_Q_template <- function(n=3, k= 1, set_indices = TRUE) {
+  if (length(n) == 1) {
+    n <- rep(n, k)
+  }
+  all_states <- do.call(expand.grid, lapply(n, \(x) 0:(x-1)))
+  dimnms <- apply(all_states, 1, \(x) sprintf("(%s)", paste(x, collapse = ",")))
+  ns <- prod(n)
+  m <- matrix(0, ns, ns)
+  for (i in 1:ns) {
+    ## exactly one state changes ...
+    for (j in 1:ns) {
+      m[i,j] <- as.numeric(sum(all_states[i,] != all_states[j,])== 1)
+    }
+  }
+  dimnames(m) <- list(dimnms, dimnms)
+  if (set_indices) {
+    m[m!=0] <- seq_len(sum(m==1))
+  }
+  return(m)
+}
+
+get_multitrait <- function(trait_number, tree) {
+  ss <- function(n) {
+    sample(0:(n - 1), size = Ntip(tree), replace = TRUE)
+  }
+  repeat {
+    d <- list()
+    for (i in seq_along(trait_number)) {
+      d[[i]] <- ss(trait_number[i])
+    }
+    d <- as.data.frame(d)
+    names(d) <- paste0("trait", seq_along(trait_number))
+    if (nrow(unique(d)) == prod(trait_number)) break
+  }
+  d <- cbind(tree$tip.label, d)
+  return(d)
+}
+
 #' @param trans_rates vector of transition rates
 #' @param Q_template a square matrix with zero values on the diagonal, and corresponding to any forbidden transitions, and integer indices in positions corresponding to distinct transition rates
 setup_Q <- function(trans_rates, Q_template) {
@@ -25,6 +68,9 @@ prune_nll <- function(pars) {
   ntips <- length(trait_values)
   ## FIXME: this can be generalized (for polymorphic/unknown states),
   ##  should possibly be moved up a level
+  if (nrow(liks) < ntips) {
+    stop("Number of columns in 'liks' is less than number of tips (ntips)")
+  }
   liks[1:ntips,] <- 0
   for (i in 1:nrow(liks)) {
     liks[i, trait_values[i]] <- 1
@@ -59,16 +105,16 @@ prune_nll <- function(pars) {
 #' }
 #' d <- cbind(ss(2), ss(3), ss(5))
 #' multitrait_to_int(d, c(2, 3, 5))
-multitrait_to_int <- function(d, n = NULL) {
+multi_to_single <- function(traits, n = NULL) {
   if (is.null(n)) {
     ## assume traits are zero-indexed
-    n <- apply(d, 2, max) + 1
+    n <- apply(traits, 2, max) + 1
   } else if (length(n) == 1) {
-    n <- rep(n, ncol(d))
+    n <- rep(n, ncol(as.data.frame(traits)))
   }
   x <- rev(cumprod(rev(n)))
   x <- c(x[-1], 1)  ## (15, 5, 1) for n = (2, 3, 5)
-  rowSums(sweep(d, MARGIN = 2, x, "*")) + 1
+  rowSums(sweep(traits, MARGIN = 2, x, "*")) + 1
 }
 
 ## TO CHECK: check against parameters? against full matrix?
@@ -106,34 +152,14 @@ hmm2prune <- function(corHMM_fit) {
   result <- prune_nll(pars)
   return(result)
 }
-
-#' @param n (vector of) number of states (if n is a scalar, all traits have the same number of states)
-#' @param k number of traits (only used if n is a scalar)
-#' @examples
-#' setup_Q_template(n = 3, k = 2)
-#' setup_Q_template(n = 2, k = 3)
-setup_Q_template <- function(n=3, k= 1, set_indices = TRUE) {
-  if (length(n) == 1) {
-    n <- rep(n, k)
-  }
-  all_states <- do.call(expand.grid, lapply(n, \(x) 0:(x-1)))
-  dimnms <- apply(all_states, 1, \(x) sprintf("(%s)", paste(x, collapse = ",")))
-  ns <- prod(n)
-  m <- matrix(0, ns, ns)
-  for (i in 1:ns) {
-    ## exactly one state changes ...
-    for (j in 1:ns) {
-      m[i,j] <- as.numeric(sum(all_states[i,] != all_states[j,])== 1)
-    }
-  }
-  dimnames(m) <- list(dimnms, dimnms)
-  if (set_indices) {
-    m[m!=0] <- seq_len(sum(m==1))
-  }
-  return(m)
-}
-
   
+##' @example 
+##' if (FALSE) {
+##' system.time(Q_big <- setup_Q_template(k=5)) ## 10 seconds
+##' png("bigmat.png")
+##' imat(Q_big)
+##' dev.off()
+##' }
 imat <- function(m, useRaster = TRUE, ...) {
   require(Matrix)
   image(Matrix(m), useRaster = useRaster, xlab = "", ylab = "", sub = "", ...)
@@ -148,21 +174,3 @@ imat <- function(m, useRaster = TRUE, ...) {
 ##   }
 ##   return(Reduce(kronecker, lapply(n, Q0)))
 ## }
-
-if (FALSE) {
-  ## testing
-  system.time(Q_big <- setup_Q_template(k=5)) ## 10 seconds
-  png("bigmat.png")
-  imat(Q_big)
-  dev.off()
-}
-
-traitM <- function(n_tips, n_traits, prob = 0.5) {
-  m <- matrix(rbinom(n_tips * n_traits, size = 1, prob = prob),
-              nrow = n_tips, ncol = n_traits)
-  
-  colnames(m) <- paste0("trait", seq_len(n_traits))
-  rownames(m) <- paste0("tip", seq_len(n_tips))
-  
-  return(m)
-}
