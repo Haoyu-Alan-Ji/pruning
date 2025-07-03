1ca2716f7b28ad39cfbe9ece94c6949a4f4c0608
Ben Bolker <bbolker@gmail.com>
2025-07-03 17:10:33 -0400

    parallel restarts etc

--- R/pruning_funs.R
+++ R/pruning_funs.R
@@ -174,3 +174,14 @@ imat <- function(m, useRaster = TRUE, ...) {
 ##   }
 ##   return(Reduce(kronecker, lapply(n, Q0)))
 ## }
+
+random_refit <- function(ff) {
+  n <- length(ff$par)
+  start <- log(abs(rnorm(n)))
+  res <- with(ff, nlminb(start, fn, gr))
+  names(start) <- paste0("start", seq_along(start))
+  fitted <-  res$par
+  names(fitted) <- paste0("fitted", seq_along(fitted))
+  with(res, data.frame(objective, convergence, message,
+                       rbind(start), rbind(fitted)))
+}
