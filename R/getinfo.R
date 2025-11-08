ec9f45079ffddaf55da6713c5da5fb8b532da9af
Alan <alanji0322@qq.com>
2025-11-08 00:44:47 -0600

    para loglik diff

--- R/getinfo.R
+++ R/getinfo.R
@@ -11,51 +11,14 @@ fitfun  <- function(dat, ..., rate.cat = 1) {
   x
 }
 
-bestfit  <- function(dat, ..., rate.cat = 1, multistart = 10, jitter.sd = 0.25) {
-  tt <- system.time(
-    if (multistart > 1) {
-      Q <- Q_template(k= ncol(dat$data)-1)
-      nrates <- sum(Q != 0)
-      p <- rexp(nrates, rate = 1)
-      dat <- list(tree = dat$tree, data = dat$data, rate.mat = Q, p = p)
-      
-      invisible(capture.output(suppressWarnings(
-        x <- with(dat, corHMM(tree, data, rate.mat, p = p, rate.cat = rate.cat,  ...)))))
-      jitter <- pmin(pmax(rnorm(multistart-1, mean = 0, sd = jitter.sd), 0), 1)
-      loglik <- list()
-      model <- list()
-      loglik[1] <- x$loglik
-      model[[1]] <- x
-      
-      for (i in 1:(multistart-1)) {
-        p <- log(jitter[i] + exp(p))
-        dat <- list(tree = dat$tree, data = dat$data, rate.mat = Q, p = p)
-        invisible(capture.output(suppressWarnings(
-          y <- with(dat, corHMM(tree, data, rate.mat, p = p, rate.cat = rate.cat, ...)))))
-        model[[i+1]] <- y
-        loglik[i+1] <- y$loglik
-      }
-      
-      best <- model[[which.max(loglik)]]
-    })
-  attr(best, "time") <- tt
-  best
-}
-
 sumfun <- function(ntrait = 2, ntaxa = 200, model = "ARD", multistart = FALSE, 
                    try.times = 10, seed = NULL, ...) {
   if (!is.null(seed)) set.seed(seed)
   seed <- seed %||% NA
   ss <- simfun(ntrait = ntrait, ntaxa = ntaxa, seed = seed)
-  if (multistart == TRUE) {
-    fit_orig <- bestfit(ss, model = model, 
-                        multistart = try.times,  ...)
-    fit_RTMB <- bestfit(ss, use_RTMB = TRUE, model = model, 
-                        multistart = try.times,  ...)
-  } else {
-    fit_orig <- fitfun(ss, model = model, ...)
-    fit_RTMB <- fitfun(ss, use_RTMB = TRUE, model = model, ...)
-    }
+  fit_orig <- fitfun(ss, model = model, ...)
+  fit_RTMB <- fitfun(ss, use_RTMB = TRUE, model = model, ...)
+  
   p_orig <- parfun(fit_orig)
   p_RTMB <- parfun(fit_RTMB)
   p_diff <- p_orig-p_RTMB
